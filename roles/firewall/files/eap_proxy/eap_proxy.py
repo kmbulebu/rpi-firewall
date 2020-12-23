28833d78d0f61053cb786ed1133a1c71b02326dc
Kevin Bulebush <kmbulebu@gmail.com>
2020-12-22 23:01:15 -0500

    Use python3 for eap_proxy

--- roles/firewall/files/eap_proxy/eap_proxy.py
+++ roles/firewall/files/eap_proxy/eap_proxy.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 usage: eap_proxy [-h] [--ping-gateway] [--ping-ip PING_IP]
                  [--ignore-when-wan-up] [--ignore-start] [--ignore-logoff]
